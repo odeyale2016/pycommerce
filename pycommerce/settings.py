2ab6215b685c6bacd669e074edf790a0436c979f
Odeyale Kehinde <odeyale_kehinde@yahoo.com>
2021-01-07 17:57:35 +0100

    creating .env file

--- pycommerce/settings.py
+++ pycommerce/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '45ts+u7gh&$@kyi_61%#1lsk&c*1#^50rbc(@c5g_k6ed4vufh'
+str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
