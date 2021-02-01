3ce230cafb26d2cda29965ac9f1092c6ba37d088
Odeyale Kehinde <odeyale_kehinde@yahoo.com>
2021-02-01 13:22:04 +0100

    adding login and register pages

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,7 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 from .models import Order, Customer
 
 class OrderForm(ModelForm):
@@ -9,3 +12,7 @@ class CustomerForm(ModelForm):
     class meta:
         model = Customer
         fields = '__all__'
+class CreateUserForm(UserCreationForm):
+    class meta:
+        model = User
+        fields = ['username','email','password1','password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,43 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from .models import *
-from .forms import OrderForm, CustomerForm
+from .forms import OrderForm, CustomerForm, CreateUserForm
 # Create your views here.
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method=='POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account created successfully for username: "+ user )
+            return redirect('login')
+    context ={'form':form}
+    return render(request, 'accounts/register.html', context)
+def loginPage(request):
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR Password is not correct')
+    context ={}
+    return render(request, 'accounts/login.html', context)
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -14,17 +47,22 @@ def home(request):
     context = {'orders': orders, 'customers':customers, 'delivered':delivered, 'pending':pending, 'total_order':total_order}
     return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
+@login_required(login_url='login')
 def customers(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
     orders = customer.order_set.all()
     total_order = orders.count()
-    context = {'customer':customer, 'orders':orders, 'total_order':total_order}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders =myFilter.qs
+    context = {'customer':customer, 'orders':orders, 'total_order':total_order, 'myFilter':myFilter}
     return render(request, 'accounts/customers.html', context)
 
+@login_required(login_url='login')
 def orders(request):
     return render(request, 'accounts/orders.html')
 
@@ -41,6 +79,7 @@ def createOrder(request, pk):
     context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
     form =OrderForm(instance=order)
@@ -52,6 +91,7 @@ def updateOrder(request, pk):
     context = {'form': form}
     return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
@@ -60,6 +100,7 @@ def deleteOrder(request, pk):
     context = {'item': order}
     return render(request, 'accounts/delete_order.html', context)
 
+@login_required(login_url='login')
 def createCustomer(request):
     form = CustomerForm()
     if request.method=='POST':
